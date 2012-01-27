daaa7640802eec352f9127aa1ea482f0c3bedf6b
Colm Troy <colmtroy@gmail.com>
2012-01-27 21:50:33 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetgest::Application.config.secret_token = '449b27deaee286e95ecb11b1919ce296d9ca86e962a254d139ff5cf0fb357b2f0f25859c9fb95474d97962cc281afe4c3f04cce4736b48aa146f1f5e5a3cece5'
